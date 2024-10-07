97a7f6813e67e5f97968089f0e33ef79156ca598
palzkx2 <palzkx2@naver.com>
2024-10-07 10:03:22 +0900

    aaaaa추가

--- src/body/Main.js
+++ src/body/Main.js
@@ -107,7 +107,7 @@ const Main = () => {
                         <img src={jkart} alt='' width={200} height={300}/>
                     </div>
                     <div style={{width:200, height:300, backgroundColor: '#e2e2e2', marginLeft:'70px', cursor:'pointer'}} onClick={scrollTosDm}>
-                        <p style={{color:'#fff'}}>스드메fdsaf</p>
+                        <p style={{color:'#fff'}}>스드메aaaaa</p>
                     </div>
                     <div style={{width:200, height:300, backgroundColor: '#e2e2e2', marginLeft:'70px', cursor:'pointer'}} onClick={scrollTohSRef}>
                         <p style={{color:'#fff'}}>혼수</p>
