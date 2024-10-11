d4008c92346dfed7229d6e623685e919ea81fff8
palzkx2 <palzkx2@naver.com>
2024-10-11 09:34:05 +0900

    깃허브 테스트

---
+++ MarryUs/src/main/java/com/spring/marryus/MarryUsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.marryus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarryUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarryUsApplication.class, args);
+	}
+
+}
---
+++ MarryUs/src/test/java/com/spring/marryus/MarryUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.marryus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarryUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
